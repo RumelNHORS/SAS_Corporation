3b7502bc384ade1455aa6ca1dbf46ea65e81da99
RumelNHORS <rumelnhors52@gmail.com>
2024-09-21 05:28:12 -0400

    create serializer for the project model.

--- sas/serializers.py
+++ sas/serializers.py
@@ -14,3 +14,9 @@ class BusinessPartnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = sas_models.BusinessPartner
         fields = ['id', 'name', 'image', 'description', 'website_link']
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = sas_models.Projects
+        fields = ['id', 'project_name', 'project_link', 'description', 'project_image']
