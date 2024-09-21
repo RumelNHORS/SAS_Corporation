0b85f678f09084a3c0e44a7d69e3990134fc1ebe
RumelNHORS <rumelnhors52@gmail.com>
2024-09-21 05:21:14 -0400

    create model for Project.

--- sas/models.py
+++ sas/models.py
@@ -5,6 +5,8 @@ class Service(models.Model):
     icon = models.ImageField(upload_to='services/icons/', blank=True, null=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -14,6 +16,18 @@ class BusinessPartner(models.Model):
     image = models.ImageField(upload_to='companies/images/', blank=True, null=True)
     description = models.TextField()
     website_link = models.URLField(max_length=200, blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name
+    
+
+class Projects(models.Model):
+    project_name = models.CharField(max_length=250, null=True, blank=True)
+    project_image = models.FileField(upload_to='projects/images', null=True, blank=True)
+    project_link = models.URLField(max_length=250, null=True, blank=True)
+    description = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    
